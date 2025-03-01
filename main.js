23667db52ab095871692a5884c3a1af9ca50d1fb
Kitocya <nu.kitonetly126@gmail.com>
2025-03-02 04:50:51 +0900

    add

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-import * as THREE from "../Three-js-master/node_modules/three/build/three.module.js"
+import * as THREE from './Three-js-master/node_modules/three/build/three.module.js';
 
-const scene = new THREE.Scene()
-console.log(scene);
+let scene, camera, renderer;
+
+scene = new THREE.Scene();
+
+camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+renderer = new THREE.WebGLRenderer({alpha:true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
