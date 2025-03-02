97070c4dc777f4ecdb2b50df6e35fd549069a3bb
Kitocya <nu.kitonetly126@gmail.com>
2025-03-03 04:44:50 +0900

    add

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-//Three.jsのインポート
+// Three.jsのインポート
 import * as THREE from './Three-js-master/node_modules/three/build/three.module.js';
 
-//グローバル宣言
+// グローバル宣言
 let scene, camera, renderer, pointLight;
 
-//シーン追加
+// シーン追加
 scene = new THREE.Scene();
 
-//カメラ追加
+// カメラ追加
 camera = new THREE.PerspectiveCamera(
     50,
     window.innerWidth / window.innerHeight,
@@ -16,35 +16,38 @@ camera = new THREE.PerspectiveCamera(
 );
 camera.position.set(0, 0, 500);
 
-//レンダラー追加
+// レンダラー追加
 renderer = new THREE.WebGLRenderer({alpha:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-renderer.render(scene, camera);
 
-//ジオメトリ（骨格）追加
+// ジオメトリ（骨格）追加
 let ballGeometry = new THREE.SphereGeometry(
     100, //半径
     64, //滑らかさ幅
     32 //滑らかさ高
 );
 
-//マテリアル（色）追加
+// マテリアル（色）追加
 let ballMaterial = new THREE.MeshPhysicalMaterial();
 
-//メッシュ化（骨格と色の組み合わせ）追加
+// メッシュ化（骨格と色の組み合わせ）追加
 let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
 scene.add(ballMesh);
 
-//平行光源追加
-let directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+// 平行光源追加
+let directionalLight = new THREE.DirectionalLight(0xffffff, 5);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
 
-//点光源追加
-pointLight = new THREE.PointLight(0xffffff, 1);
+// 点光源追加
+pointLight = new THREE.PointLight(0xffffff, 2);
 pointLight.position.set(-200, -200, -200);
 scene.add(pointLight);
 
-//レンダリング追加
-renderer.render(scene, camera);
+// アニメーションループ追加
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
